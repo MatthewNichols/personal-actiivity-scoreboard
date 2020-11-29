d90acc7ac5747761b10da0c5aa5411b9385f140e
Matthew Nichols <matthew@matthew-nichols.com>
2020-11-29 22:47:02 +0100

    Edited Activities

--- client/src/data/activities.ts
+++ client/src/data/activities.ts
@@ -6,9 +6,10 @@ export interface Activity {
 
 export const ActivityList: Activity[] = [
     { id: "stairs", name: "Stairs 15 flights", points: 30 },
-    { id: "pushups-20", name: "20 Pushups", points: 20 },
-    { id: "pushups-10", name: "10 Pushups", points: 10 },
     { id: "bear-crawl", name: "Bear Crawl", points: 5 },
+    { id: "reps-20", name: "20 Reps", points: 20 },
+    { id: "reps-10", name: "10 Reps", points: 10 },
+    { id: "reps-5", name: "5 Reps", points: 5 },
     { id: "cardio-20", name: "20 Mins Cardo", points: 30 },
     { id: "1hr-standing", name: "1 Hr Standing", points: 10 },
     { id: "stretching", name: "Stretching 5 mins", points: 10 }
