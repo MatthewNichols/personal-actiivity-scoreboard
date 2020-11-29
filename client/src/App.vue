3b0dd30fd72045bb6eec6fe15d5c660edcca7435
Matthew Nichols <matthew@matthew-nichols.com>
2020-11-29 13:18:30 +0100

    Adding Montserrat

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,8 +3,10 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+ 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
